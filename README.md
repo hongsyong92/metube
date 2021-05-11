ce3ff1409a8e0b1196503f14d20c8b444e647115
SYONG <ghdgus24@naver.com>
2021-05-11 15:59:18 +0900

    router 기초공사시작

---
+++ README.md
@@ -0,0 +1,18 @@
+# Metube
+
+Global Router
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+User Router
+/users/edit -> Edit User
+/users/delete -> Delete User
+
+Video Router
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete Comment
--- src/server.js
+++ src/server.js
@@ -1,38 +1,28 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-// 어떤 메소드로 어떤 url 로 가는지 알려주는 미들웨어
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("next");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("home");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("private");
-};
-
-// app.use를 맨위에 놓고 미들웨어를 주면 모든 url에서 미들웨어 사용가능
 app.use(logger);
-app.use(privateMiddleware);
 
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`server listening on port http://localhost:${PORT} 🚀`);
