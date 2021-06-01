709cbf170363d38168dc9c14fec82140b1fc86d5
SYONG <ghdgus24@naver.com>
2021-06-01 18:07:15 +0900

    pug 세팅

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,38 @@
 // 글로벌 라우터에서 쓰이는 중
 // trending -> 메인에서 보여줄 트렌드 영상들
-export const trending = (req, res) => res.send("Trending Videos");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1
+    }
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 
 // 비디오 라우터에서 쓰이는 중
-export const see = (req, res) => {
-  return res.send(`Watch Videos #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send("Edit Videos");
-};
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("Upload Videos");
 export const deleteVideo = (req, res) => {
