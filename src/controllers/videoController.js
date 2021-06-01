25269881f278bf815af1b35ee4da7bce65db9701
SYONG <ghdgus24@naver.com>
2021-06-01 10:28:35 +0900

    라우터 및 컨트롤러 세팅

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,16 @@
 // 글로벌 라우터에서 쓰이는 중
+// trending -> 메인에서 보여줄 트렌드 영상들
 export const trending = (req, res) => res.send("Trending Videos");
 
 // 비디오 라우터에서 쓰이는 중
-export const watch = (req, res) => res.send("Watch Videos");
-export const edit = (req, res) => res.send("Edit Videos");
+export const see = (req, res) => {
+  return res.send(`Watch Videos #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  return res.send("Edit Videos");
+};
+export const search = (req, res) => res.send("Search Videos");
+export const upload = (req, res) => res.send("Upload Videos");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Videos");
+};
