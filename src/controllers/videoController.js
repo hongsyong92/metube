22ef82b7bb12334e0f5010ad5cfed84bd8567fea
SYONG <ghdgus24@naver.com>
2021-06-03 18:03:24 +0900

    model 추가중

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,40 +1,35 @@
+import Video from "../models/Video";
+
 // 글로벌 라우터에서 쓰이는 중
-// trending -> 메인에서 보여줄 트렌드 영상들
-export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1
-    }
-  ];
+// home -> 메인에서 보여줄 트렌드 영상들
+export const home = async (req, res) => {
+  const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
 
 // 비디오 라우터에서 쓰이는 중
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search Videos");
-export const upload = (req, res) => res.send("Upload Videos");
-export const deleteVideo = (req, res) => {
-  return res.send("Delete Videos");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  return res.render("watch", { pageTitle: `Watching`, video });
+};
+
+// getEdit은 edit 화면을 보여주는(render) 함수
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  return res.render("edit", { pageTitle: `Editing`, video });
+};
+// postEdit은 edit 화면에서의 변경사항을 저장해주는 함수
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  // 이 함수에서 업로드한 비디오를 videos 배열에 추가
+  const { title } = req.body;
+  return res.redirect("/");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,20 @@
 import express from "express";
-import { edit, see, upload, deleteVideo } from "../controllers/videoController";
+import {
+  getEdit,
+  watch,
+  postEdit,
+  getUpload,
+  postUpload
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 // (\\d+) 는 숫자만 가져오는 정규식
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", watch);
+
+// 아래는 ~~edit url 에 접근했을 때, get method면 getEdit 함수 사용,
+// post method면 postEdit 함수 사용한다는 것.
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
